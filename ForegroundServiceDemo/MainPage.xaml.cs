d46658dd1d01b273e516faca7cd08a09d97f46cb
Victor <freelance2@eviciti.com.mx>
2024-12-05 23:49:22 -0600

    Agregar archivos de proyecto.

---
+++ ForegroundServiceDemo/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ForegroundServiceDemo.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ForegroundServiceDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnForegroundService_Click(object sender, EventArgs e)
+        {
+            if (DependencyService.Resolve<IForegroundService>().IsForeGroundServiceRunning())
+            {
+                App.Current.MainPage.DisplayAlert("Opps", "El servicio en primer plano ya se está ejecutando", "OK");
+            }
+            else
+            {
+                DependencyService.Resolve<IForegroundService>().StartMyForegroundService();
+            }
+
+
+        }
+
+        private void btnStopForegroundService_Click(object sender, EventArgs e)
+        {
+            DependencyService.Resolve<IForegroundService>().StopMyForegroundService();
+        }
+    }
+}
